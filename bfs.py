d449caa696b949101402bdbb495f88a9db5ff4f4
Emre Ergeldi <bangfuzzy@gmail.com>
2023-12-23 14:41:54 +0300

    first commit

---
+++ bfs.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def bfs(graph, start_node, target_node):
+    visited = set()
+    queue = deque([(start_node, [start_node])])
+
+    while queue:
+        current_node, path = queue.popleft()
+
+        if current_node == target_node:
+            return path
+
+        if current_node not in visited:
+            visited.add(current_node)
+            neighbors = graph[current_node]['neighbors']
+            for neighbor in neighbors:
+                neighbor_id = neighbor['id']
+                if neighbor_id not in visited:
+                    queue.append((neighbor_id, path + [neighbor_id]))
+
+    return None
