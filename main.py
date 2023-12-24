43939b2766e4904bb582b1319801c128d103ff5e
47dwarves <ruzgarbasbay@gmail.com>
2023-12-24 15:53:59 +0300

    dfs and bfs combined in the same file. lack of turkish characters fixed and timers added for said functions.

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-from bfs import bfs
-from dfs import dfs
+import time
+
+from bfs_dfs import bfs, dfs
 from graph import graph
 
 
 def main():
-    start_node = int(input("Baslangic sehrini giriniz: "))
-    target_node = int(input("Hedef sehri giriniz: "))
+    start_node = input("Başlangıç şehrini giriniz: ")
+    target_node = input("Hedef şehri giriniz: ")
 
     # BFS
+    bfs_start_time = time.perf_counter_ns()
     result_bfs = bfs(graph, start_node, target_node)
+    bfs_end_time = time.perf_counter_ns()
 
     if result_bfs:
-        print("BFS Yol:", result_bfs)
+        print(f"BFS Yol: {result_bfs}")
+        print(f"BFS Zaman: {bfs_end_time - bfs_start_time} ns")
     else:
-        print("BFS Yolu bulunamadi.")
+        print("BFS Yolu bulunamadı.")
 
     # DFS
+    dfs_start_time = time.perf_counter_ns()
     result_dfs = dfs(graph, start_node, target_node)
+    dfs_end_time = time.perf_counter_ns()
 
     if result_dfs:
-        print("DFS Yol:", result_dfs)
+        print(f"DFS Yol: {result_dfs}")
+        print(f"DFS Zaman: {dfs_end_time - dfs_start_time} ns")
     else:
-        print("DFS Yol bulunamadi.")
+        print("DFS Yol bulunamadı.")
 
 
 if __name__ == "__main__":
